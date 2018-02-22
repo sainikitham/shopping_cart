e6a9e237497e1d076473b2e89fdf15d59533510e
Nikitha <a614343@fmr.com>
2018-02-22 19:03:32 +0530

    edit-new routing

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -9,7 +9,7 @@ import { RecipeService } from '../../recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
-
+  @Input() index: number;
    constructor(private _RecipeService: RecipeService) { }
 
   ngOnInit() {
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit} from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,10 +11,14 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
-  constructor(private _RecipeService: RecipeService) { }
+  constructor(private _RecipeService: RecipeService,
+          private _Router: Router,
+        private _route: ActivatedRoute) { }
 
   ngOnInit() {
     this.recipes = this._RecipeService.getrecipe();
   }
-
+  onnewrecipe() {
+   this._Router.navigate(['new'], {relativeTo: this._route});
+ }
 }
